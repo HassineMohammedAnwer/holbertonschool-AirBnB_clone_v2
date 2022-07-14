824ea244fe247f421dce293a6f3628a4cb070455
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 03:39:44 +0100

    03-00fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-# Bash script to setup a web server.
-
+# sets up your web servers for the deployment of web_static
 apt-get update -y
 apt-get install -y nginx
 service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "simple contest to test my Nginx conf" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location http://finalcountdown.tech/hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
+exit 0
