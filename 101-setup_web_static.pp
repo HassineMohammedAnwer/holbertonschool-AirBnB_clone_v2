8fe01f570139956864800f93a0e16f09d6773256
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 09:06:42 +0100

    a03-101fixxx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -21,6 +21,12 @@ exec {'ech':
 exec {'lnking':
   command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
 }
+
+$cont2 = '\nserver {\n	listen 80 default_server;\n	server_name _;\n	location / {\n		try_files \$uri \$uri/ =404;\n	}\n	location /hbnb_static {\n		alias /data/web_static/current;\n		index index.html;\n		try_files \$uri \$uri /hbnb_static/index.html;\n	}\n}\n'
+
+exec {'serverr':
+  command => '/usr/bin/env echo $cont > /etc/nginx/sites-available/default',
+}
 exec {'restrating':
   command => '/usr/bin/env service nginx restart',
 }
