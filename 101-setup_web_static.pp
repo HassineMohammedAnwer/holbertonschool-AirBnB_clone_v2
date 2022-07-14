31f7b61e9ae5197aae32d7e4f3e5cebcbb8d0285
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 07:26:32 +0100

    a03-101fx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,24 +11,38 @@ service {'nginx':
 
 $cont = '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>'
 
-file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test'] :
+file { '/data':
   ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  mode   => '777',
-  require =>  Package['nginx']
+  mode   => '0777',
+}
+
+file { '/data/web_static':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode   => '0751',
+}
+
+file { ['/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test']:
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode   => '0751',
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  mode   => '0777',
+  force  => true
 }
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => $cont,
   require => Package['nginx']
-}
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-  force  => true
 }
 
 service {'nginx':
