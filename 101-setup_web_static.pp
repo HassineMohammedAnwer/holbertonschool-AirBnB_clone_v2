c9d8c9ef05420f19186dbd0c6364afb6f2ce3071
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 07:07:57 +0100

    a03-101fx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,43 +1,36 @@
 #using puppet setup web static like task 0
 
-class nginx {
-
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
-  service {'nginx':
-    ensure  =>  'running',
-    require => file_line['ADDING A LOCATION']
-  }
-
+package { 'nginx':
+  ensure => 'installed',
 }
 
-class creation {
+service {'nginx':
+  ensure  =>  'running',
+  require => file_line['ADDING A LOCATION']
+}
 
-  $cont = '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>'
+$cont = '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>'
 
-  file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test'] :
-    ensure  => 'directory',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    require =>  Package['nginx']
-  }
+file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test'] :
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require =>  Package['nginx']
+}
 
-  file { '/data/web_static/releases/test/index.html':
-    ensure  => 'present',
-    content => $cont,
-    require =>  Package['nginx']
-  }
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => $cont,
+  require =>  Package['nginx']
+}
 
-  file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test',
-    force  => true
-  }
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  force  => true
+}
 
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
+service {'nginx':
+  ensure  =>  'running',
+  require => file_line['ADDING A LOCATION']
 }
