ac1e71282c77dfb5bc0e1153776a370fddbad462
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 03:41:51 +0100

    03-00fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location http://finalcountdown.tech/hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location localhost/hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
