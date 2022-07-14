e4a5910a0f4dde031d7b95eed6241cc6b9973a69
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-14 04:05:34 +0100

    A03-01

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  generates a .tgz archive from web_static folder """
+
+from datetime import datetime
+from fabric.api import local
+import os
+
+def do_pack():
+    """ function do_pack """
+    try:
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    path = "versions/web_static_{}.tgz".format(time)
+    cmd = "tar -cvzf {} web_static".format(path)
+    local("mkdir -p  versions")
+    local(cmd)
+    except:
+        return None
+    return path
