19ce421818ab5d14b125823146eb7781c40b4f73
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2022-07-14 15:05:30 +0100

    05fixxx

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,9 +36,9 @@ def _number(n):
     return '{:d} is a number'.format(n)
 
 
-app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def _number_template(n):
-    """ number_template """
+    """ /number_template/n """
     return render_template('5-number.html', number=n)
 
 
